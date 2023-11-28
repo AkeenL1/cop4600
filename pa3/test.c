b10176ff8b174ae195e74059fc366a68f840672e
akeenl-sp <akeen.lewis@smartrent.com>
2023-11-28 17:34:37 -0500

    adjusting test to read and write

--- pa3/test.c
+++ pa3/test.c
@@ -1,15 +1,3 @@
-/**
- * @file   test.c
- * @author Derek Molloy
- * @date   7 April 2015
- * @version 0.1
- * @brief  A Linux user space program that communicates with the charkmod.c LKM. It passes a
- * string to the LKM and reads the response from the LKM. For this example to work the device
- * must be called /dev/charkmod.
- * @see http://www.derekmolloy.ie/ for a full description and follow-up descriptions.
- *
- * Adapted for COP 4600 by Dr. John Aedo
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -17,49 +5,49 @@
 #include <string.h>
 #include <unistd.h>
 
-#define BUFFER_LENGTH 256           ///< The buffer length (crude but fine)
-static char receive[BUFFER_LENGTH]; ///< The receive buffer from the LKM
+#define BUFFER_LENGTH 256             // The buffer length
+static char receive[BUFFER_LENGTH];  // The receive buffer from the LKM
 
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
-        printf("Usage: test <path to device>\n");
-        exit(0);
-    }
-    char *devicepath = argv[1];
-
-    int ret, fd;
+int main() {
+    int ret, fd_writer, fd_reader;
     char stringToSend[BUFFER_LENGTH];
-    strncpy(stringToSend, "", sizeof(stringToSend));
-    printf("Starting device test code example...\n");
-    fd = open(devicepath, O_RDWR); // Open the device with read/write access
-    if (fd < 0)
-    {
-        perror("Failed to open the device...");
+
+    // Open the writer device
+    fd_writer = open("/dev/lkmasg2_writer", O_WRONLY);
+    if (fd_writer < 0){
+        perror("Failed to open the writer device...");
         return errno;
     }
+
+    // Open the reader device
+    fd_reader = open("/dev/lkmasg2_reader", O_RDONLY);
+    if (fd_reader < 0){
+        perror("Failed to open the reader device...");
+        close(fd_writer); // Close the already opened writer device
+        return errno;
+    }
+
     printf("Type in a short string to send to the kernel module:\n");
-    scanf("%[^\n]%*c", stringToSend); // Read in a string (with spaces)
-    printf("Writing message to the device [%s].\n", stringToSend);
-    ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
-    if (ret < 0)
-    {
+    scanf("%[^\n]%*c", stringToSend);  // Read in a string (with spaces)
+    printf("Writing message to the writer device: [%s].\n", stringToSend);
+    ret = write(fd_writer, stringToSend, strlen(stringToSend)); // Send the string to the LKM
+    if (ret < 0){
         perror("Failed to write the message to the device.");
         return errno;
     }
 
-    printf("Press ENTER to read back from the device...\n");
+    printf("Press ENTER to read back from the reader device...\n");
     getchar();
 
-    printf("Reading from the device...\n");
-    ret = read(fd, receive, BUFFER_LENGTH); // Read the response from the LKM
-    if (ret < 0)
-    {
+    printf("Reading from the reader device...\n");
+    ret = read(fd_reader, receive, BUFFER_LENGTH); // Read the response from the LKM
+    if (ret < 0){
         perror("Failed to read the message from the device.");
         return errno;
     }
     printf("The received message is: [%s]\n", receive);
-    printf("End of the program\n");
+
+    close(fd_reader);
+    close(fd_writer);
     return 0;
 }
